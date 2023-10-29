e4f8fe335d14302615e2b109bbdb57a8b7b654de
Andrés Muñoz <andresestebanmu@gmail.com>
2023-10-28 22:33:24 -0300

    login

--- src/routes/Navbar.jsx
+++ src/routes/Navbar.jsx
@@ -3,7 +3,7 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import { Link } from 'react-router-dom';
-import Login from '../components/login';
+import Login from '../../src/components/Login';
 
 
 function Navegation() {
